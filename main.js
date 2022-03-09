4949a21f40bbbcd699d76986e1523f4c5defefa5
blablass <82516957+blablass@users.noreply.github.com>
2022-03-09 18:28:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,16 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 function modelloaded(){
     console.log("modelloaded");
 }
+function idenimg(){
+    img = document.getElementById("anyid")
+    classifier.classify(img,getresult)
+}
+function getresult(error,result){
+    if(error){
+        console.log(error)
+    }else{
+        console.log(result)
+        document.getElementById("Object_name").innerHTML = result[0].label
+        document.getElementById("accuracy").innerHTML = result[0].confidence.toFixed(3)
+    }
+}
